c6251b65c9e0ea263330e01478cda121a0558941
Ke Zhang <kz2249@columbia.edu>
2022-07-12 18:03:37 +0800

    Update install.sh

--- continuous_integration/scripts/install.sh
+++ continuous_integration/scripts/install.sh
@@ -4,7 +4,6 @@ set -vxeuo pipefail
 # These packages are installed in the base environment but may be older
 # versions. Explicitly upgrade them because they often create
 # installation problems if out of date.
-pip install Cython
 python -m pip install --upgrade pip setuptools wheel numpy
 # Versioneer uses the most recent git tag to generate __version__, which appears
 # in the published documentation.
