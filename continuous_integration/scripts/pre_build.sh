6135bf4e6b910d703039ab3bd6ce129b2f64adc9
Ke Zhang <kz2249@columbia.edu>
2022-08-12 15:40:53 +0800

    Update pre_build.sh

--- continuous_integration/scripts/pre_build.sh
+++ continuous_integration/scripts/pre_build.sh
@@ -1,7 +1,6 @@
 set -xe
 
-locate apt-get
-/usr/bin/apt-get update
-/usr/bin/apt-get install git build-essential cmake libeigen3-dev valgrind
+apt-get update
+apt-get install git build-essential cmake libeigen3-dev valgrind
 ln -s /usr/include/eigen3/Eigen /usr/include/Eigen
 ls /usr/include      
