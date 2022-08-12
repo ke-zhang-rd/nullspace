ede3eb9664119df846f6112d9706f0754d672927
Ke Zhang <kz2249@columbia.edu>
2022-08-13 02:19:05 +0800

    Update pre_build.sh

--- continuous_integration/scripts/pre_build.sh
+++ continuous_integration/scripts/pre_build.sh
@@ -1,6 +1,5 @@
-set -xe
+set -x
 
-ls /usr/include 
+ls -la /usr/include 
 yum -y install eigen3-devel
-ln -s /usr/include/eigen3/Eigen /usr/include/Eigen
-ls /usr/include
+# ln -s /usr/include/eigen3/Eigen /usr/include/Eigen
