20df67c3af862337bdb1ccbdb7f626d246ea26ca
Ke Zhang <kz2249@columbia.edu>
2022-08-17 00:35:40 +0800

    Update macos_install_eigen.sh

--- continuous_integration/scripts/macos_install_eigen.sh
+++ continuous_integration/scripts/macos_install_eigen.sh
@@ -1,7 +1,5 @@
 set -x
 
 brew install eigen
-ln -s /usr/local/Cellar/eigen/3.3.4/include/eigen3/Eigen /usr/local/include/Eigen
-echo | gcc -E -Wp,-v -
-ls -la /usr/local/include
-ls /usr/local/include/Eigen
+sudo ln -s /usr/local/include/eigen3/Eigen /usr/local/include/Eigen
+ls -la /usr/local/include/Eigen
