85587a2855e25f891b198eb00bac3b4b88f6c0c4
ke-zhang-rd <kz2249@columbia.edu>
2022-08-30 01:41:17 +0800

    MNT

--- nullspace/tests/test_abc.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import nullspace
-
-
-def test_xyz():
-    x = np.random.random((3))
--- nullspace/tests/test_xyz.py
+++ nullspace/tests/test_xyz.py
@@ -1,7 +1,6 @@
 import numpy as np
 from nullspace.svd_wrapper import xyz
 
-
 def test_xyz():
     x = np.random.random((3))
     xyz(x)
