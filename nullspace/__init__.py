f8aacf2779092df0e2b9c98d4d7b1d42b8a0a773
ke-zhang-rd <kz2249@columbia.edu>
2025-02-24 19:16:55 +0800

    Update __init__.py

--- nullspace/__init__.py
+++ nullspace/__init__.py
@@ -279,5 +279,6 @@ def autodiff_svd(A, q_star, *, symbolic=False):
                 np.array(ds_tensor.subs(q_star), dtype=float),
                 np.array(dv_tensor.subs(q_star), dtype=float))
 
+
 from . import _version
 __version__ = _version.get_versions()['version']
